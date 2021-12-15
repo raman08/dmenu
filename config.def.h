37b91418602955416aac0ab66b5f594c1ae8f86f
Raman Preet Singh <ramanpre0810@gmail.com>
2021-12-15 20:05:24 +0530

    Added Lineheight patch

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+	"JetBrains Mono SemiBold::size=10",
+	//"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
@@ -28,6 +28,9 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
